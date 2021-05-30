6f6a5a854973901958711dc18e8f8743f8d333b5
vadimmos <vadimmos@ya.ru>
2021-05-30 15:38:11 +0300

    add vados-canvas

---
+++ vados-canvas.js
@@ -0,0 +1,44 @@
+export class VadosCanvas {
+  /**@param {HTMLElement} container */
+  constructor(container) {
+    this.container = container;
+
+    this.cnv = document.createElement('canvas');
+    this.ctx = this.cnv.getContext('2d');
+
+    this.cnv.style.setProperty('box-shadow',
+      `0px 2px 5px rgba(0,0,0,.25),
+      0px -2px 5px rgba(0,0,0,.25),
+      2px 0px 5px rgba(0,0,0,.25),
+      -2px 0px 5px rgba(0,0,0,.25)
+    `);
+    this.cnv.style.setProperty('width', '100%');
+    this.cnv.style.setProperty('height', '100%');
+
+    this.container.appendChild(this.cnv);
+
+    this.cnv.width = this.cnv.offsetWidth;
+    this.cnv.height = this.cnv.offsetHeight;
+
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(0, 0, this.cnv.width, this.cnv.height);
+
+    const down = () => {
+      this.cnv.addEventListener('mousemove', move);
+      this.cnv.addEventListener('mouseup', up);
+      this.ctx.beginPath();
+    };
+    const move = (evt) => {
+      this.ctx.lineTo(evt.x, evt.y);
+      this.ctx.stroke();
+    };
+    const up = () => {
+      this.cnv.removeEventListener('mousemove', move);
+      this.cnv.removeEventListener('mouseup', up);
+    };
+
+    this.ctx.strokeStyle = 'black';
+
+    this.cnv.addEventListener('mousedown', down);
+  }
+}
