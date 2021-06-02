764cc5875491b25c69ff3d16845078f45455e87a
vadimmos <vadimmos@ya.ru>
2021-06-02 20:42:51 +0300

    add VRange

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {VCanvas} from './v-canvas.js';
-import { VButton } from './v-tool.js';
+import { VButton, VRange } from './v-tool.js';
 import { VToolbar } from './v-toolbar.js';
 const container = document.getElementById('container');
 const canvas = new VCanvas(container);
@@ -7,5 +7,9 @@ const toolBar = new VToolbar(canvas, container);
 
 const greenBtn = new VButton({ label: 'green', action: () => { canvas.setStrokeStyle('green') }});
 toolBar.bar.appendChild(greenBtn.control);
+
 const blackBtn = new VButton({ label: 'black', action: () => { canvas.setStrokeStyle('black') }});
 toolBar.bar.appendChild(blackBtn.control);
+
+const range = new VRange({ label: 'size', action: () => { canvas.setLineWidth(range.value) } });
+toolBar.bar.appendChild(range.control);
--- v-tool.js
+++ v-tool.js
@@ -1,13 +1,13 @@
-export class VTool{
-  constructor({ id = uuidv4(), name = id || '', label = name ||''}){
+export class VTool {
+  constructor({ id = uuidv4(), name = id || '', label = name || '' }) {
     this.id = id;
     this.name = name;
     this.label = label;
     /**@type {HTMLElement} */
     this._control = null;
   }
-  get control(){
-    if (!this._control){
+  get control() {
+    if (!this._control) {
       this._control = document.createElement('div');
       this._control.textContent = this.label;
     }
@@ -15,8 +15,8 @@ export class VTool{
   }
 }
 
-export class VButton extends VTool{
-  constructor({action = () => {console.warn(`no action in ${this.name}`)}, ...props}){
+export class VButton extends VTool {
+  constructor({ action = (e) => { console.warn(`no action in ${this.name}`) }, ...props }) {
     super(props);
     this.action = action;
     /**@type {HTMLButtonElement} */
@@ -27,6 +27,31 @@ export class VButton extends VTool{
       this._control = document.createElement('button');
       this._control.textContent = this.label;
       this._control.addEventListener('click', this.action);
+    }
+    return this._control;
+  }
+}
+export class VRange extends VButton {
+  constructor({ min = 0, max = 100, step = 1, ...props }) {
+    super(props);
+    this.min = min;
+    this.max = max;
+    this.step = step;
+    /**@type {HTMLInputElement} */
+    this._control = null;
+    this.value = 0;
+  }
+  get control() {
+    if (!this._control) {
+      this._control = document.createElement('input');
+      this._control.type = 'range';
+      this._control.min = String(this.min);
+      this._control.max = String(this.max);
+      this._control.step = String(this.step);
+      this._control.addEventListener('input', (e) => {
+        this.value = parseFloat(this._control.value);
+        this.action(e);
+      });
     }
     return this._control;
   }
